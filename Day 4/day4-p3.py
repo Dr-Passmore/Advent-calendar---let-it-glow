a1b45353539111750f7446cd5f8cc031eebc9f5a
dr-passmore <phillipspassmore@gmail.com>
2024-12-31 18:55:32 +0000

    Day 4 part 3 completed

---
+++ Day 4/day4-p3.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import time
+
+seg1 = Pin(13, Pin.OUT)
+seg2 = Pin(12, Pin.OUT)
+seg3 = Pin(11, Pin.OUT)
+seg4 = Pin(10, Pin.OUT)
+seg5 = Pin(9, Pin.OUT)
+
+segments = [seg1, seg2, seg3, seg4, seg5]
+
+while True:
+    for led in segments:
+        led.value(1)
+        time.sleep(0.08)
+        led.value(0)
+
+    for led in reversed (segments):
+        led.value(1)
+        time.sleep(0.08)
+        led.value(0)
