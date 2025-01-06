6c30f2337b24b2dd3bbbb10161baa39579713e1d
dr-passmore <phillipspassmore@gmail.com>
2025-01-06 19:43:10 +0000

    Day 8 part 2 completed

---
+++ Day 8/day8-p2.py
@@ -0,0 +1,18 @@
+from machine import Pin
+from neopixel import NeoPixel
+import time
+
+ring = NeoPixel(Pin(2), 12)
+
+myleds = [0, 3, 6, 9]
+
+ring.fill((0, 0, 0))
+
+ring.write()
+
+time.sleep(1)
+
+for i in myleds:
+    ring[i] = (0, 0, 10)
+    ring.write()
+   
